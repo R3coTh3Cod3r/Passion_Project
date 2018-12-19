0bcef6c5bbe328464ada0f2e115f8586b61e3643
Mareco Nabors <naborsmarico99@gmail.com>
2018-12-19 16:24:03 -0600

    text & game

--- Pyton_Game_files/sample.py
+++ Pyton_Game_files/sample.py
@@ -1,51 +1,75 @@
 import pygame
+
 pygame.init()
 
 
-displaySurface = pygame.display.set_mode((500,480))
+displaySurface = pygame.display.set_mode((1014,658))
 pygame.display.set_caption('My Game')
 # animation for walking (INCLUDE ANIMATION SPRITES)
 walkRight = [pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R1.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R2.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R3.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R4.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R5.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R6.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R7.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R8.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/R9.png')]
 walkLeft = [pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L1.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L2.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L3.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L4.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L5.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L6.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L7.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L8.png'), pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/L9.png')]
 
-backGround = pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passionate_Project/screen.png')
+backGround = pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/pixelbackground_small.png')
 Character = pygame.image.load('/home/student/CodeSchool/IdeaProjects/Passion_Project/Pyton_Game_files/images/standing.png')
 
 clock = pygame.time.Clock()
 
 
-x = 50
-y = 400
-width = 64
-height = 64
-vel = 5
-isJump = False
-jumpCount = 10
-left = False
-right = False
-walkCount = 0
+
+class player(object):
+    def __init__(self,x,y,width,height):
+       self.x = x
+       self.y = y
+       self.width = width
+       self.height = height
+       self.vel = 5
+       self.isJump = False
+       self.left = False
+       self.right = False
+       self.walkCount = 0
+       self.jumpCount = 10
+
+
+    def draw(self,displaySurface):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            displaySurface.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            displaySurface.blit(walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount +=1
+        else:
+            displaySurface.blit(Character, (self.x, self.y))
+
+
+
+
+
 
 
 def rePlayWindow():
-    global walkCount
+    pygame.font.init() # you have to call this at the start,
+
+    #This will initialize the text
+    myfont = pygame.font.SysFont('Comic Sans MS', 120)
+
+
+    textsurface = myfont.render('Quest Hunter', False, (0, 0, 0))
+
     displaySurface.blit(backGround, (0,0))
 
-    if walkCount + 1 >= 20:
-        walkCount = 0
+    # This will display the text on the screen
+    displaySurface.blit(textsurface, (300,100))
 
-    if left:
-        displaySurface.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right:
-        displaySurface.blit(walkRight[walkCount//3], (x,y))
-        walkCount +=1
-    else:
-        displaySurface.blit(Character, (x,y))
-
+    warrior.draw(displaySurface)
+    pygame.transform.scale(displaySurface, (0, 0))
     pygame.display.update()
 
 
 #mainloop
+warrior = player(500, 500, 64, 64)
 run = True
 while run:
     clock.tick(27)
@@ -56,35 +80,35 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-        left = True
-        right = False
-    elif keys[pygame.K_RIGHT] and x < 500 - width - vel:
-        x += vel
-        right = True
-        left = False
+    if keys[pygame.K_LEFT] and warrior.x > warrior.vel:
+        warrior.x -= warrior.vel
+        warrior.left = True
+        warrior.right = False
+    elif keys[pygame.K_RIGHT] and warrior.x < 1800 - warrior.width - warrior.vel:
+        warrior.x += warrior.vel
+        warrior.right = True
+        warrior.left = False
     else:
-        right = False
-        left = False
-        walkCount = 0
+        warrior.right = False
+        warrior.left = False
+        warrior.walkCount = 0
 
-    if not(isJump):
+    if not(warrior.isJump):
         if keys[pygame.K_SPACE]:
-            isJump = True
-            right = False
-            left = False
-            walkCount = 0
+            warrior.isJump = True
+            warrior.right = False
+            warrior.left = False
+            warrior.walkCount = 0
     else:
-        if jumpCount >= -10:
+        if warrior.jumpCount >= -10:
             neg = 1
-            if jumpCount < 0:
+            if warrior.jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2) * 0.5 * neg
-            jumpCount -= 1
+            warrior.y -= (warrior.jumpCount ** 2) * 0.5 * neg
+            warrior.jumpCount -= 1
         else:
-            isJump = False
-            jumpCount = 10
+            warrior.isJump = False
+            warrior.jumpCount = 10
 
     rePlayWindow()
 
